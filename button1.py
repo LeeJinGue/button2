507ba3502e56ad6f7490a7077c3b20151d013f38
LeeJinGue <hippo9851@gmail.com>
2017-11-15 14:21:52 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,9 @@ window =Tk()
 
 b1 = Button(window, text="이진규")
 b2 = Button(window, text="60162165")
+b3 = Button(window, text="12/02")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
-
+b3.grid(row=1, column=0)
 window.mainloop()
